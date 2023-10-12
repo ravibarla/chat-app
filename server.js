058c22ef6f0cddab598659bcbb98f254ed7edecf
ravi barla <ravibarla1@gmail.com>
2023-10-12 21:01:42 +0530

    establishing socket connection

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { Server } from "socket.io";
+import cors from "cors";
+import http from "http";
+
+const app = express();
+
+//1. create server
+const server = http.createServer(app);
+
+//2. create socket server
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+//3. use socket events
+
+io.on("connection", (socket) => {
+  console.log("connection is established");
+  socket.on("disconnect", () => {
+    console.log("connection is disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("app is listening in port : 3000");
+});
